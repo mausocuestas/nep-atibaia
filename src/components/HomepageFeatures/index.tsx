3780f6e4f2348838429db5eaa6f046fbea971c95
mausocuestas <alcatoobr@yahoo.com.br>
2024-09-21 17:37:22 -0300

    cards com link na Home

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,56 +1,60 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
+  link: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Objetivo',
+    title: 'Solicitações Pendentes',
     Svg: require('@site/static/img/target.svg').default,
     description: (
       <>
-        Estágio na área da saúde é onde o conhecimento se transforma 
-        em cuidado, e a teoria ganha vida no bem-estar de cada paciente.
+        Visualize e gerencie todas as solicitações pendentes de estágio.
       </>
     ),
+    link: 'docs/intro',
   },
   {
-    title: 'Benefícios',
+    title: 'Novas Solicitações',
     Svg: require('@site/static/img/plane-take-off.svg').default,
     description: (
       <>
-        O estágio na saúde é o momento em que você cresce não apenas como profissional, 
-        mas como ser humano, aprendendo a salvar vidas enquanto constrói a sua própria carreira.
+        Crie e submeta novas solicitações de estágio na área da saúde.
       </>
     ),
+    link: 'docs/intro',
   },
   {
-    title: 'Suporte',
+    title: 'Todas as Solicitações',
     Svg: require('@site/static/img/support.svg').default,
     description: (
       <>
-        No estágio na área da saúde, você não caminha sozinho – é guiado por profissionais 
-        experientes que oferecem apoio, conhecimento e a confiança necessária para enfrentar desafios reais.
+        Acesse o histórico completo de todas as solicitações de estágio.
       </>
     ),
+    link: 'docs/intro',
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg, description, link}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+      <Link to={link} className={clsx(styles.featureLink, styles.featureCard)}>
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+        <div className="text--center padding-horiz--md">
+          <Heading as="h3">{title}</Heading>
+          <p>{description}</p>
+        </div>
+      </Link>
     </div>
   );
 }
