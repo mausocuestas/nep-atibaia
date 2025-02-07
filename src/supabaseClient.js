d6ce595a818c0e471c11d6787f307a612b1a3e78
mausocuestas <alcatoobr@yahoo.com.br>
2025-02-07 16:30:37 -0300

    primeira conexão supa ô glória1

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jvruhstbelqikpsemven.supabase.co'; // Substitua pelo URL do seu projeto
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2cnVoc3RiZWxxaWtwc2VtdmVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MzIwMDksImV4cCI6MjA1NDUwODAwOX0.IoeSSjEuQAw07ZuzVl1y7Q6gsxKrXiI-xCRWR4OOcW4'; // Substitua pela sua chave pública (anon)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2cnVoc3RiZWxxaWtwc2VtdmVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MzIwMDksImV4cCI6MjA1NDUwODAwOX0.IoeSSjEuQAw07ZuzVl1y7Q6gsxKrXiI-xCRWR4OOcW4'
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
